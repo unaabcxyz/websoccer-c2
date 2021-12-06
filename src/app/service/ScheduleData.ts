dd3d2504e3e23f4228196a7001b7e243e8f7d293
dwiauliaa <dwiauliiaaaa@gmail.com>
2021-12-06 16:05:35 +0700

    selesai

--- src/app/service/ScheduleData.ts
+++ src/app/service/ScheduleData.ts
@@ -2,19 +2,19 @@ import { Schedule } from "src/interface/schedule";
 import { Team } from "src/interface/team";
 
 export const SEASON_SCHEDULE:Schedule [] = [
-    {id: 1, PlayingDate: new Date(2021,08,10), HomeTeam: 'Persija', 
+    {id: 1, PlayingDate: new Date(2021,8,10), HomeTeam: 'Persija', 
     AwayTeam: 'Persib', HomeScore:3, AwayScore:4, 
     RefName:'Joko', notes:'Pertandingan overtime'},
-    {id: 2, PlayingDate: new Date(2021,08,11), HomeTeam: 'Persipon', 
+    {id: 2, PlayingDate: new Date(2021,8,11), HomeTeam: 'Persipon', 
     AwayTeam: 'Persib', HomeScore:3, AwayScore:5, 
     RefName:'John', notes:'Pertandingan undertime'},
-    {id: 3, PlayingDate: new Date(2021,08,12), HomeTeam: 'Persi', 
+    {id: 3, PlayingDate: new Date(2021,8,12), HomeTeam: 'Persi', 
     AwayTeam: 'Persib', HomeScore:3, AwayScore:1, 
     RefName:'Jo', notes:'Pertandingan ontime'},
-    {id: 4, PlayingDate: new Date(2021,08,13), HomeTeam: 'Evos', 
+    {id: 4, PlayingDate: new Date(2021,8,13), HomeTeam: 'Evos', 
     AwayTeam: 'Persib', HomeScore:3, AwayScore:9, 
     RefName:'ko', notes:'Pertandingan overtime'},
-    {id: 5, PlayingDate: new Date(2021,08,14), HomeTeam: 'MU', 
+    {id: 5, PlayingDate: new Date(2021,8,14), HomeTeam: 'MU', 
     AwayTeam: 'Persib', HomeScore:3, AwayScore:3, 
     RefName:'Jk', notes:'Pertandingan ontime'}
 ]
